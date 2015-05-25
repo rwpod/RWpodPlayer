d77e54b7280ce1b3a5a7245dbd0d20447ec0728d
Alexey Vasiliev <leopard.not.a@gmail.com>
2015-05-25 13:03:50 +0300

    add tabs bar

--- App/Components/Main.js
+++ App/Components/Main.js
@@ -4,45 +4,90 @@ var React = require("react-native");
 var {
   Text,
   View,
-  StyleSheet
+  StyleSheet,
+  TabBarIOS
 } = React;
-var Api = require("../Utils/Api");
+var Podcasts = require("./Podcasts")
+
 
 var styles = StyleSheet.create({
-  mainContainer: {
+  tabContent: {
     flex: 1,
-    padding: 30,
-    marginTop: 65,
-    flexDirection: "column",
-    justifyContent: "center",
-    backgroundColor: "#48BBEC"
-  }
+    alignItems: "center"
+  },
+  tabText: {
+    color: "white",
+    margin: 50
+  },
 });
 
+
+
 class Main extends React.Component {
-  constructor(props) {
+  static title: "<TabBarIOS>";
+  static description: "Tab-based navigation";
+
+  constructor (props) {
     super(props);
-    this.handlePodcastsResponse = this.handlePodcastsResponse.bind(this);
-    this.handlePodcastsError = this.handlePodcastsError.bind(this);
-    /* get podcasts */
-    Api.getPodcasts()
-      .then((res) => this.handlePodcastsResponse(res))
-      .catch((err) => this.handlePodcastsError(err));
+    this.state = {
+      selectedTab: 'blueTab',
+      notifCount: 0,
+      presses: 0
+    }
   }
 
-  handlePodcastsError (err) {
-    console.log("Error", err);
+  _renderPodcasts () {
+    return (
+      <Podcasts />
+    )
   }
 
-  handlePodcastsResponse (res) {
-    console.log("Result", res);
+  _renderContent (color: string, pageText: string) {
+    return (
+      <View style={[styles.tabContent, {backgroundColor: color}]}>
+        <Text style={styles.tabText}>{pageText}</Text>
+        <Text style={styles.tabText}>{this.state.presses} re-renders of the More tab</Text>
+      </View>
+    );
   }
 
   render () {
     return (
-      <View style={styles.mainContainer}>
-        <Text>Testing the Router</Text>
-      </View>
+      <TabBarIOS>
+        <TabBarIOS.Item
+          systemIcon="featured"
+          selected={this.state.selectedTab === 'blueTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'blueTab',
+            });
+          }}>
+          {this._renderPodcasts()}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          systemIcon="history"
+          badge={this.state.notifCount > 0 ? this.state.notifCount : undefined}
+          selected={this.state.selectedTab === 'redTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'redTab',
+              notifCount: this.state.notifCount + 1,
+            });
+          }}>
+          {this._renderContent('#783E33', 'Red Tab')}
+        </TabBarIOS.Item>
+        <TabBarIOS.Item
+          systemIcon="more"
+          selected={this.state.selectedTab === 'greenTab'}
+          onPress={() => {
+            this.setState({
+              selectedTab: 'greenTab',
+              presses: this.state.presses + 1
+            });
+          }}>
+          {this._renderContent('#21551C', 'Green Tab')}
+        </TabBarIOS.Item>
+      </TabBarIOS>
     );
   }
 }
---
+++ App/Components/Podcasts.js
@@ -0,0 +1,56 @@
+"use strict";
+
+var React = require("react-native");
+var {
+  Text,
+  View,
+  StyleSheet
+} = React;
+var Api = require("../Utils/Api");
+
+var styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 30,
+    marginTop: 65,
+    flexDirection: "column",
+    justifyContent: "center",
+    backgroundColor: "#48BBEC"
+  }
+});
+
+class Podcasts extends React.Component {
+  constructor(props) {
+    super(props);
+    /* binds */
+    this.loadPodcasts = this.loadPodcasts.bind(this);
+    this.handlePodcastsResponse = this.handlePodcastsResponse.bind(this);
+    this.handlePodcastsError = this.handlePodcastsError.bind(this);
+    /* get podcasts */
+    this.loadPodcasts();
+  }
+
+  loadPodcasts () {
+    Api.getPodcasts()
+      .then((res) => this.handlePodcastsResponse(res))
+      .catch((err) => this.handlePodcastsError(err));
+  }
+
+  handlePodcastsError (err) {
+    console.log("Error", err);
+  }
+
+  handlePodcastsResponse (res) {
+    console.log("Result", res);
+  }
+
+  render () {
+    return (
+      <View style={styles.mainContainer}>
+        <Text>Testing the Router</Text>
+      </View>
+    );
+  }
+}
+
+module.exports = Podcasts;

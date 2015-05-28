26145924d2d3fa6b78be10a68f31c66d79f3eb59
Alexey Vasiliev <leopard.not.a@gmail.com>
2015-05-28 22:51:12 +0300

    show podcasts

--- index.ios.js
+++ index.ios.js
@@ -4,23 +4,59 @@ var React = require("react-native");
 var {
   AppRegistry,
   NavigatorIOS,
+  TabBarIOS,
   StyleSheet
 } = React;
-var Main = require("./App/Components/Main");
+var Podcasts = require("./App/Components/Podcasts");
 
 
 class RWpodPlayer extends React.Component {
-  render () {
+  static title: "<TabBarIOS>";
+  static description: "Tab-based navigation";
+
+  constructor (props) {
+    super(props);
+    /* binds*/
+    this._renderPodcasts = this._renderPodcasts.bind(this);
+    this._selectTab = this._selectTab.bind(this);
+    this._renderPodcasts = this._renderPodcasts.bind(this);
+    /* state */
+    this.state = {
+      selectedTab: 'podcasts'
+    }
+  }
+
+  _renderPodcasts () {
     return (
       <NavigatorIOS
         style={styles.container}
         initialRoute={{
-          component: Main,
-          title: "RWpod Player"
+          component: Podcasts,
+          title: "Podcasts"
         }}
       />
+    )
+  }
+
+  render () {
+    return (
+      <TabBarIOS>
+        <TabBarIOS.Item
+          systemIcon="featured"
+          selected={this.state.selectedTab === 'podcasts'}
+          onPress={() => this._selectTab('podcasts')}>
+          {this._renderPodcasts()}
+        </TabBarIOS.Item>
+      </TabBarIOS>
     );
   }
+
+  _selectTab (tabName) {
+    this.setState({
+      selectedTab: tabName
+    });
+  }
+
 }
 
 
@@ -28,7 +64,15 @@ var styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#111111"
-  }
+  },
+  tabContent: {
+    flex: 1,
+    alignItems: "center"
+  },
+  tabText: {
+    color: "white",
+    margin: 50
+  },
 });
 
 
